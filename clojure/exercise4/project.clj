dfa6a1aedff3df720e0ba44d44094329c42b960b
Tim Carter <twrcarter@hotmail.co.uk>
2016-02-28 15:02:44 +0000

    clojure 4 - replace local function with the one from common

--- clojure/exercise4/project.clj
+++ clojure/exercise4/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [common "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot exercise4.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- clojure/exercise4/src/exercise4/core.clj
+++ clojure/exercise4/src/exercise4/core.clj
@@ -1,19 +1,14 @@
 (ns exercise4.core
+  (:require common.core)
   (:gen-class))
-
-(defn get-input[prompt]
-  (do
-    (print prompt)
-    (flush)
-    (read-line)))
 
 (defn -main
   "exercise 4 - silly story"
   [& args]
   (do
     (let
-      [noun (get-input "Enter a noun: ")
-      verb (get-input "Enter a verb: ")
-      adjective (get-input "Enter an adjective: ")
-      adverb (get-input "Enter an adverb: ")]
+      [noun (common.core/get-input "Enter a noun: ")
+      verb (common.core/get-input "Enter a verb: ")
+      adjective (common.core/get-input "Enter an adjective: ")
+      adverb (common.core/get-input "Enter an adverb: ")]
       (println "Do you" verb "your" adjective noun (str adverb "? How funny ...")))))
