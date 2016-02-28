cd2e795b92544e9bd4fefe847d4157e3714f59d5
Tim Carter <twrcarter@hotmail.co.uk>
2016-02-28 17:09:11 +0000

    clojure ex6 use an alias for common

--- clojure/exercise6/src/exercise6/core.clj
+++ clojure/exercise6/src/exercise6/core.clj
@@ -1,5 +1,6 @@
 (ns exercise6.core
-  (:require common.core [clj-time.core :as t])
+  (:require [common.core :as i]
+            [clj-time.core :as t])
   (:gen-class))
 
 (defn -main
@@ -7,8 +8,8 @@
   [& args]
     (do
       (let
-        [age-in-years (common.core/get-input "What is your age?: ")
-        retirement-age-in-years (common.core/get-input "How old at retirement?: ")
+        [age-in-years (i/get-input "What is your age?: ")
+        retirement-age-in-years (i/get-input "How old at retirement?: ")
         this-year (t/year (t/now))
         retirement-year (+ this-year (- retirement-age-in-years age-in-years))]
         (println "It's" (str this-year ", so you can retire in " retirement-year)))))
